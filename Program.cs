b5918a60a5b8b3f1fb99dc206add833eaa99b29b
hasankahraman <hasankahraman06092@gmail.com>
2022-01-12 00:07:17 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int ay = DateTime.Now.Month;
+
+//açıklama
+switch (ay)
+{
+    case 1:
+        Console.WriteLine("Ocak");
+        break;
+    case 2:
+        Console.WriteLine("Şubat");
+        break;
+    case 3:
+        Console.WriteLine("Mart");
+        break;
+    case 4:
+        Console.WriteLine("Nisan");
+        break;
+    case 5:
+        Console.WriteLine("Mayıs");
+        break;
+    case 6:
+        Console.WriteLine("Haziran");
+        break;
+    case 7:
+        Console.WriteLine("Temmuz");
+        break;
+    case 8:
+        Console.WriteLine("Ağustos");
+        break;
+    case 9:
+        Console.WriteLine("Eylül");
+        break;
+    case 10:
+        Console.WriteLine("Ekim");
+        break;
+    case 11:
+        Console.WriteLine("Kasım");
+        break;
+    case 12:
+        Console.WriteLine("Aralık");
+        break;
+    default:
+    Console.WriteLine("Hatalı veri girişi.");
+    break;
+}
+
+switch (ay)
+{
+    case 12:
+    case 1:
+    case 2:
+        Console.WriteLine("Kış");
+        break;
+   
+    case 3:
+    case 4:
+    case 5:
+        Console.WriteLine("İlkBahar");
+        break;
+    
+    case 6:
+    case 7:
+    case 8:
+        Console.WriteLine("Yaz");
+        break;
+    
+    case 9:
+    case 10:
+    case 11:
+        Console.WriteLine("Sonbahar");
+        break;
+    
+    default:
+    Console.WriteLine("Hatalı veri girişi.");
+    break;
+}
